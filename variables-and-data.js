085d88ab7212afa678253d72ca12e9d3152222c6
Dylan DeRaad <dyljdr@gmail.com>
2020-06-28 17:02:15 -0600

    add conditionals-and-functions.js objects-apis.js variables-and-data.js

---
+++ variables-and-data.js
@@ -0,0 +1,21 @@
+/*
+*Variables, Data Types,and Typing
+ */
+let wordData = "Word Data";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+let obj = {
+    key1: "Value",
+    key2: 3,
+    boolean: true,
+    obj2: {
+        obj2Key1: "Internal Object Value",
+    }
+}
+
+console.log(obj.key1);
